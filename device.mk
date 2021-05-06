68d2b188b04b900f4441885b0cfb8edbb61928d4
Martin Štrobl <recordyletsplays@gmail.com>
2021-05-06 23:29:20 +0200

    gauguin: Build kernel from source
    Used source: https://github.com/Sap1k/kernel_xiaomi_gauguin

--- device.mk
+++ device.mk
@@ -110,10 +110,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl \
     $(LOCAL_PATH)/keylayout/lito-lagoonqrd-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/lito-lagoonqrd-snd-card_Button_Jack.kl
 
-# Kernel
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/dtb.img:dtb.img
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_gauguin
