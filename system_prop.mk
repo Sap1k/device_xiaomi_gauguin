f6db03e7ce2f845407aa9722b8717ddeca1e2139
Martin Štrobl <recordyletsplays@gmail.com>
2021-05-14 13:56:53 +0200

    gauguin: Disable blurs by default

--- system_prop.mk
+++ system_prop.mk
@@ -26,7 +26,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1  
+    ro.sf.blurs_are_expensive=1 \
+    persist.sys.sf.disable_blurs=1
 
 # Fingerprint Payment for China apps
 PRODUCT_PRODUCT_PROPERTIES += \
